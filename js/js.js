96ccf5087f77eb2a1a14f31a19ae3ee2b7bdfed7
6u6uka <6u6uka999@mail.ru>
2017-10-19 00:27:33 +0400

    first commit

---
+++ js/js.js
@@ -0,0 +1,22 @@
+ $('.sl').slick({
+ 	autoplay: true,
+ 	autoplaySpeed: 3000,
+  	arrows: false,
+ 	dots: true,
+ 	slidesToShow: 5,
+ 	slidesToScroll: 5,
+ 	
+
+ });
+ $('.sl_insta').slick({
+ 	slidesToShow: 5,
+ 	slidesToScroll: 5,
+ 	arrows: false,
+ 	
+ })
+ $('.sl_girls').slick({
+ 	autoplay: true,
+ 	autoplaySpeed: 2000,
+ 	arrows: false,
+ 	fade: true,
+ })
