6066a178491be1bccb6cf6aac00acb0e6bab9186
6u6uka <6u6uka999@mail.ru>
2017-10-25 23:06:53 +0400

    more pages

--- js/js.js
+++ js/js.js
@@ -13,10 +13,15 @@
  	slidesToScroll: 5,
  	arrows: false,
  	
- })
+ });
  $('.sl_girls').slick({
  	autoplay: true,
  	autoplaySpeed: 2000,
  	arrows: false,
  	fade: true,
- })
+ });
+$('.lookbook2017').slick({
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: false,
+});
